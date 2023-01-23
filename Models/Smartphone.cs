b359567ddcca3a9811d577be01e5d367b9e7a0d0
gsolazaro <gsolazaro@gmail.com>
2023-01-23 13:32:05 -0300

    Desafio POO concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
